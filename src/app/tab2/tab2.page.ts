b6d1a55ec11fa220cdadfcdf9c6e3a7598cba260
Statsenko <smooker14@gmail.com>
2020-06-10 12:49:20 +0300

    commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 
+const defaultAvatarUrl = 'https://ionicframework.com/docs/demos/api/avatar/avatar.svg';
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public users = [
+    {
+      name: 'Андрей Стеценко',
+      avatar: defaultAvatarUrl,
+    },
+    {
+      name: 'Анастасия Земляная',
+      avatar: defaultAvatarUrl,
+    },
+  ];
+  public filteredUsers = [...this.users];
 
   constructor() {}
+
+  public searchUsers(value: string): void {
+    if (!value.length) {
+      this.filteredUsers = [...this.users];
+    }
+
+    this.filteredUsers = this.users.filter(option => option.name.toLowerCase().indexOf(value.toLowerCase()) > -1);
+  }
 
 }
