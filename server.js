113f6c10c41be138a51113d5b055c883355d8d8e
Statsenko <smooker14@gmail.com>
2020-06-10 11:30:51 +0300

    commit

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require('express'),
+    app = express();
 
-const app = express();
+app.use(express.static('www'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/www'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/www/index.html'));
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// API Routes
+// app.get('/blah', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
