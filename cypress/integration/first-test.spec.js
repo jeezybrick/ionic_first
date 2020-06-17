cd25d7dcbcae1fc2463da01f4cff3529e730ab8c
Statsenko <smooker14@gmail.com>
2020-06-17 19:05:14 +0300

    commit

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,5 +1,8 @@
 describe("First test", () => {
     it("should visit login page", () => {
-        cy.visit("http://localhost:8100/auth/login");
+        cy.visit("/auth/login");
+        cy.get('#email').type('123@gmail.com');
+        cy.get('#password').type('123456', {timeout: 3000});
+        cy.get('.submit-button').click();
     });
 });
