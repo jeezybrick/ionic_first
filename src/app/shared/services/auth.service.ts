809d412aa5b97f4061bc4c957d4c6d12700ea908
andrey <smooker14@gmail.com>
2020-11-13 18:06:56 +0200

    commit

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -8,104 +8,85 @@ import { TokenStorage } from './token.storage';
 import { filter, tap } from 'rxjs/operators';
 import { User } from '../models/user.model';
 import { Socket } from 'ngx-socket-io';
-import { Board } from '../models/board.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  public $userSource: Subject<any> = new BehaviorSubject(null);
-  public user: User;
+    public $userSource: Subject<any> = new BehaviorSubject(null);
+    public user: User;
 
-  constructor(private socket: Socket, private http: HttpClient, private token: TokenStorage, private boardService: BoardService) {
-    this.me().subscribe((data) => {
-      this.user = data.user;
-      this.socket.emit('set-id', data.user._id);
-    });
-  }
+    constructor(private socket: Socket, private http: HttpClient, private token: TokenStorage, private boardService: BoardService) {
+        this.me().subscribe((data) => {
+            this.user = data.user;
+            this.socket.emit('set-id', data.user._id);
+        });
+    }
 
-  login(email: string, password: string): Observable<any> {
-    return new Observable(observer => {
-      this.http.post('/api/auth/login', {
-        email,
-        password
-      }).subscribe((data: any) => {
+    login(email: string, password: string): Observable<any> {
+        return this.http.post('/api/auth/login', {
+            email,
+            password
+        }).pipe(
+            tap(data => {
+                this.setUser(data.user);
+                this.token.saveToken(data.token);
+                this.boardService.getBoards().subscribe();
+                this.boardService.startPollingInviteToBoard();
+            }),
+        );
+    }
 
-        observer.next({user: data.user});
-        this.setUser(data.user);
-        this.token.saveToken(data.token);
-        this.boardService.getBoards().subscribe();
-        this.boardService.startPollingInviteToBoard();
-        observer.complete();
-
-      }, (error) => {
-        observer.error({message: error.error.message, status: error.status});
-        observer.complete();
-      });
-    });
-  }
-
-  register(fullname: string, email: string, password: string, repeatPassword: string): Observable<any> {
-    return new Observable(observer => {
-      this.http.post('/api/auth/register', {
+    register(fullname: string, email: string, password: string, repeatPassword: string): Observable<any> {
+      return this.http.post('/api/auth/register', {
         fullname,
         email,
         password,
         repeatPassword
-      }).subscribe((data: any) => {
+      }).pipe(
+          tap(data => {
+            this.setUser(data.user);
+            this.token.saveToken(data.token);
+          }),
+      );
+    }
 
-        observer.next({user: data.user});
-        this.setUser(data.user);
-        this.token.saveToken(data.token);
-        observer.complete();
-      }, (error) => {
-        observer.error({message: error.error.message, status: error.status});
-        observer.complete();
-      });
-    });
-  }
+    setUser(user): void {
+        this.$userSource.next(user);
+    }
 
-  setUser(user): void {
-    this.$userSource.next(user);
-  }
+    getUser(): Observable<any> {
+        return this.$userSource.asObservable().pipe(filter(item => !!item));
+    }
 
-  getUser(): Observable<any> {
-    return this.$userSource.asObservable().pipe(filter(item => !!item));
-  }
+    updateProfile(data): Observable<any> {
+        return this.http.patch(`/api/user/edit`, data).pipe(
+            tap(user => {
+                this.setUser(user);
+            }),
+        );
+    }
 
-  updateProfile(data): Observable<any> {
-    return new Observable(observer => {
-      return this.http.patch(`/api/user/edit`, data).subscribe((user: any) => {
-        observer.next(user);
-        this.setUser(user);
-        observer.complete();
-      }, (error) => {
-        observer.error(error.error);
-        observer.complete();
-      });
-    });
-  }
+    uploadAvatar(file: File): Observable<boolean> {
+        const formData = new FormData();
+        formData.append('avatar', file, file.name);
 
-  uploadAvatar(file: File): Observable<boolean> {
-    const formData = new FormData();
-    formData.append('avatar', file, file.name);
+        return this.http.post<boolean>(`/api/users/${this.user._id}/upload-avatar`, formData);
 
-    return this.http.post<boolean>(`/api/users/${this.user._id}/upload-avatar`, formData);
+    }
 
-  }
+    me(): Observable<any> {
+        return this.http.get('/api/auth/me').pipe(
+            tap(data => {
+                this.setUser(data.user);
+            }),
+        );
+    }
 
-  me(): Observable<any> {
-    return this.http.get('/api/auth/me').pipe(
-        tap(data => {
-          this.setUser(data.user);
-        }),
-    );
-  }
-
-  signOut(): void {
-    this.token.signOut();
-    this.setUser(null);
-    this.user = null;
-    this.boardService.stopPollingInviteToBoard();
-  }
+    signOut(): void {
+        this.token.signOut();
+        this.setUser(null);
+        this.user = null;
+        this.boardService.stopPollingInviteToBoard();
+    }
 }
